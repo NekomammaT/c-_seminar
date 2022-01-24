e37dd1994f0c64c28fb02f7befbfaf1ec3515220
yuichiro <yuichiro.t.tada@gmail.com>
2022-01-24 16:47:59 +0900

    test

--- test.cpp
+++ test.cpp
@@ -3,20 +3,7 @@
 
 using namespace std;
 
-vector<double> f(vector<double> v) { // 2番目の要素だけ2倍
-  v[1] *= 2;
-  return v;
-}
-
 int main()
 {
   vector<double> v1{1,2,3};
-  vector<double> v2;
-
-  cout << v1[0] << ' ' << v1[1] << ' ' << v1[2] << endl;
-
-  v2 = f(v1);
-
-  cout << v1[0] << ' ' << v1[1] << ' ' << v1[2] << endl; // v1 は値渡ししたので変わらない
-  cout << v2[0] << ' ' << v2[1] << ' ' << v2[2] << endl;
 }
